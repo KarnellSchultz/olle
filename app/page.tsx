d3caca0c25064dd8a8131601d2db3b8f0b7848f1
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-04-05 09:17:40 +0200

    add custom profile pic and heading

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,23 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 
-const URL = 'https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clg21v31v008c01un629t7whk/master'
+const HYGRAPH_URL = 'https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clg21v31v008c01un629t7whk/master'
+const hygraph = new GraphQLClient(HYGRAPH_URL);
+
+type Post = {
+  id: string;
+  title: string;
+  subtitle: string;
+  slug: string;
+  date: string;
+}
+
+type Profile = {
+  heading: string;
+  profilePic: {
+    url: string;
+  }
+}
 
 const PostsQuery = `
 query PostsQuery {
@@ -17,15 +33,15 @@ query PostsQuery {
 }
 `
 
-type Post = {
-  id: string;
-  title: string;
-  subtitle: string;
-  slug: string;
-  date: string;
-}
+const ProfileQuery = `query ProfileQuery {
+  profiles {
+    heading
+    profilePic {
+      url
+    }
+  }
+}`
 
-const hygraph = new GraphQLClient(URL);
 
 const getPosts = async () => {
   const { posts } = await hygraph.request(
@@ -35,25 +51,30 @@ const getPosts = async () => {
   return posts
 }
 
-export function generateMetadata() {
-  return { title: 'Olle Blog' };
+const getProfile = async () => {
+  const { profiles } = await hygraph.request(
+    ProfileQuery,
+  ) as { profiles: Profile[] };
+  return profiles[0]
 }
 
+
 export default async function Page() {
-  const posts = await getPosts();
+  const [posts, profile] = await Promise.all([getPosts(), getProfile()])
+
   return (
     <div>
       <h1 className='capitalize text-3xl font-bold py-6  tracking-tighter'>olle blog</h1>
       <section className='flex py-4 items-center gap-4' >
         <Image className='rounded-3xl'
-          src="/images/olle-pic.png"
+          src={profile.profilePic.url}
           width={70}
           height={70}
           alt="profile picture" />
-        <p>The personal blog of olle darmell</p>
+        <p>{profile.heading}</p>
       </section>
       <ul>
-        {posts.reverse().map((post) => (
+        {posts.reverse().map((post: Post) => (
           <li key={post.id} className='py-4'>
             <Link href={`/blog/${post.slug}`}>
               <p className='prose-2xl font-bold capitalize text-green-500'> {post.title}</p>
@@ -65,4 +86,8 @@ export default async function Page() {
       </ul>
     </div>
   );
+}
+
+export function generateMetadata() {
+  return { title: 'Olle Blog' };
 }
